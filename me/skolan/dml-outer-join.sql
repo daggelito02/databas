548c31cec36693ce28e2f207592090bea954e9fe
daggelito02 <daggelito02@gmail.com>
2025-03-08 12:45:06 +0100

    Outer join, rader som inte matchar

---
+++ me/skolan/dml-outer-join.sql
@@ -0,0 +1,37 @@
+--
+-- Outer join
+--
+
+-- We look at which teachers are responsible for at least one course.
+
+SELECT DISTINCT
+    akronym AS Akronym,
+    CONCAT(fornamn, " ", efternamn) AS Namn
+    FROM v_planering
+    ORDER BY akronym
+;
+     
+-- Outer join, include teachers without teaching
+
+SELECT DISTINCT
+    l.akronym AS Akronym,
+    CONCAT(l.fornamn, " ", l.efternamn) AS Namn,
+    l.avdelning AS Avdelning,
+    kt.kurskod AS Kurskod
+FROM larare AS l
+    LEFT OUTER JOIN kurstillfalle AS kt
+        ON l.akronym = kt.kursansvarig
+;
+
+-- Courses without course opportunities
+
+SELECT DISTINCT
+    k.kod AS Kurskod,
+    k.namn AS Kursnamn,
+    kt.lasperiod AS Läsperiod
+FROM kurs AS k
+    LEFT OUTER JOIN kurstillfalle AS kt
+        ON k.kod = kt.kurskod
+WHERE kt.lasperiod IS NULL;
+
+
